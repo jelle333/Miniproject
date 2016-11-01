ee3e7add542c6f82fae5e39a0dd46f06b4cdc1e8
baronowen <owenashby@hotmail.nl>
2016-11-01 14:23:40 +0100

    Heb hulp nodig met de get(), hij werkt niet bij mij

--- inlog aanbieders.py
+++ inlog aanbieders.py
@@ -51,7 +51,7 @@ Button(reg, text='Ga naar startpagina', command=lambda:raise_frame(start), width
 Button(reg, text='Ga naar inlog pagina', command=lambda:raise_frame(inlog), width=25).pack(side=LEFT, padx=2)
 
 Label(aanbiederGUI, text='Pagina van de aanbieder').pack()
-Button(aanbiederGUI, text='Log uit', command=lambda:raise_frame(inlog), width=25).pack(padx=2)
+Button(aanbiederGUI, text='Log uit', command=lambda:raise_frame(inlog), width=25).pack(side=BOTTOM, pady=2, padx=2)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -105,6 +105,32 @@ buttonReg = Button(reg,
                 text='Registreer',
                 width=22,
                 command=clickedReg).pack(pady=4)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+button1 = Button(aanbiederGUI,
+                text='nog niet aangeboden films')
+button1.pack(pady=10,
+             padx=5,
+             side = LEFT)
+
+button2 = Button(aanbiederGUI,
+                text='overzicht films die ik aanbied',)
+button2.pack(pady=10,
+             padx=5,
+             side = LEFT)
+
+button3 = Button(aanbiederGUI,
+                text='overzicht bezoekers bij mij',)
+button3.pack(pady=10,
+             padx=5,
+             side = LEFT)
+
+button4 = Button(aanbiederGUI,
+                text='aanmeldcode bezoekers',)
+button4.pack(pady=10,
+             padx=5,
+             side = LEFT)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
